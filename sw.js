6b06db0cf0a5870adcb38652aae7a68a5dd9f183
binjo-sys <langatemmanuel817@gmail.com>
2026-09-11 07:57:51 +0300

    Update service worker for Kenya Trucker

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-const CACHE='novella-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin){const copy=n.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return n}).catch(()=>caches.match('./index.html'))));
+const CACHE='kenya-trucker-v1';
+const ASSETS=['./','./index.html','./styles.css','./game.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./'))));
+});
