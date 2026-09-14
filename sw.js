5025360630db1d1c52828ac414709318cf663fd3
binjo-sys <langatemmanuel817@gmail.com>
2026-09-14 16:51:25 +0300

    Update PWA cache for interactive movie

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='kenya-trucker-v1';
-const ASSETS=['./','./index.html','./styles.css','./game.js','./manifest.webmanifest'];
+const CACHE='goalora-movie-v2';
+const ASSETS=['./','./index.html','./styles.css','./movie.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
   if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./'))));
