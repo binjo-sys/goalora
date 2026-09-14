19cff97ad67563d6a016b598d81d25b48dd9b1a2
binjo-sys <langatemmanuel817@gmail.com>
2026-09-14 17:05:07 +0300

    Refresh movie cache so latest cinematic changes appear

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
-const CACHE='goalora-movie-v2';
-const ASSETS=['./','./index.html','./styles.css','./movie.js','./manifest.webmanifest'];
+const CACHE='goalora-movie-v4';
+const ASSETS=['./','./index.html','./styles.css?v=4','./cinema.css?v=4','./cinema-overrides.css?v=1','./movie.js?v=4','./sound.js?v=1','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  const u=new URL(e.request.url);
-  if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./'))));
-});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));});
